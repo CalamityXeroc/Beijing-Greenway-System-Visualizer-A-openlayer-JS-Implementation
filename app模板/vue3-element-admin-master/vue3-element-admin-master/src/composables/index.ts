40b1aa85dcf4cea33610ef64e3e2ff725c8a6e83
Auto Commit <test@example.com>
2026-02-24 13:31:32 +0800

    清理：从git追踪中移除skills、app模板等隐私文件夹

--- app模板/vue3-element-admin-master/vue3-element-admin-master/src/composables/index.ts
+++
@@ -1,7 +0,0 @@
-// WebSocket 服务
-export { setupWebSocket, cleanupWebSocket } from "./websocket";
-export { useStomp, useDictSync, useOnlineCount } from "./websocket";
-export type { DictMessage, DictChangeMessage, DictChangeCallback } from "./websocket";
-
-// 表格相关
-export { useTableSelection } from "./table/useTableSelection";
