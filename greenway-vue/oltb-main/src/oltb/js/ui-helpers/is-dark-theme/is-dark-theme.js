2ec3dc4d08cf9e8b0955fb743a43d2c63cfbdc67
CalamityXeroc <2749955791@qq.com>
2026-03-11 21:41:33 +0800

    删除无用内容

--- greenway-vue/oltb-main/src/oltb/js/ui-helpers/is-dark-theme/is-dark-theme.js
+++
@@ -1,7 +0,0 @@
-import { ConfigManager } from '../../toolbar-managers/config-manager/config-manager';
-
-const isDarkTheme = function() {
-    return window.document.body.classList.contains(ConfigManager.getConfig().className.dark);
-}
-
-export { isDarkTheme };
--- greenway-vue/oltb-main/src/oltb/js/ui-helpers/is-row-direction/is-row-direction.js
+++
@@ -1,7 +0,0 @@
-import { ConfigManager } from '../../toolbar-managers/config-manager/config-manager';
-
-const isHorizontal = function() {
-    return window.document.body.classList.contains(ConfigManager.getConfig().className.row);
-}
-
-export { isHorizontal };
